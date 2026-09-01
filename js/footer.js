aac5a8dfc04d29be37307d247875fd497e790fbd
Samuel Roa <sroa@MacBook-Pro-de-Samuel.local>
2026-08-31 22:54:57 -0400

    Presentar capacidades completas de CAMI

--- js/footer.js
+++ js/footer.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <h4>Servicios</h4>
         <ul>
           <li><a href="servicios.html#facturador">Facturador Electrónico</a></li>
-          <li><a href="servicios.html#analisis-itbis">Análisis de ITBIS</a></li>
+          <li><a href="servicios.html#analisis-itbis">CAMI</a></li>
           <li><a href="servicios.html#chatbot">Chatbot 606 y 607</a></li>
           <li><a href="servicios.html#dominio">Servidor de Dominio</a></li>
           <li><a href="servicios.html#correo">Servidor de Correo</a></li>
